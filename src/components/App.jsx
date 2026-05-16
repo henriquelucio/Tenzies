b5863e2ecec14483515d811db38af752dda73366
Henrique <henriquelucio95@hotmail.com>
2026-05-16 11:46:12 -0300

    Random Array

---
+++ src/components/App.jsx
@@ -0,0 +1,28 @@
+import React from "react"
+import Die from "/src/components/Die.jsx"
+
+export default function App(){
+    const [dice, setDice] = React.useState(allNewDice)
+
+    function allNewDice(){
+        return new Array(10)
+            .fill(0)
+            .map(() => Math.ceil(Math.random() * 6))
+        /* let arrayRndNumb = []
+        for (let i = 0; i < 10; i++) {
+            let rndNumber = Math.floor(Math.random() * 6)+1
+            arrayRndNumb.push(rndNumber)
+        }
+        return arrayRndNumb */
+    }
+
+    const diceElements = dice.map(num => <Die value={num}/>)
+
+    return(
+        <main>
+            <div className="dice-container">
+                {diceElements}
+            </div>
+        </main>
+    )
+}
